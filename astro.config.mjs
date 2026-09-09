9d3b54b17530ee442a441c08f856ffec10135f30
metaory <metaory@gmail.com>
2026-09-09 10:50:05 +0330

    updated default theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import sitemap from '@astrojs/sitemap'
+import sitemap from "@astrojs/sitemap";
 
 /** @type {import('astro').AstroUserConfig} */
 export default {
-  output: 'static',
-  site: 'https://limitak-inc.github.io',
-  base: '/',
+  output: "static",
+  site: "https://limitak-inc.github.io",
+  base: "/",
   integrations: [sitemap()],
-  build: {
-    format: 'directory',
+  devToolbar: {
+    enabled: false,
   },
-}
+  build: {
+    format: "directory",
+  },
+};
